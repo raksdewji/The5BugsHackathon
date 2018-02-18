15df14521419ea49ad8ab0069091cbba08d6be8c
Rakheem Dewji <rdewj142@mtroyal.ca>
2018-02-18 12:25:54 -0700

    added android app

---
+++ app/src/main/java/com/example/the5bug/the5bugs/Settings.java
@@ -0,0 +1,16 @@
+package com.example.the5bug.the5bugs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+
+    }
+}
